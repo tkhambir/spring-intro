a58bc80b476d64bf40b79b69406f69d3c12cfa49
tkhambir <tatiana.khambir@gmail.com>
2020-10-19 09:51:07 +0300

    homework 45 solution

---
+++ src/main/java/com/spring/intro/dao/impl/UserDaoImpl.java
@@ -0,0 +1,47 @@
+package com.spring.intro.dao.impl;
+
+import com.spring.intro.dao.UserDao;
+import com.spring.intro.model.User;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserDaoImpl implements UserDao {
+    private final SessionFactory sessionFactory;
+
+    public UserDaoImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public void add(User user) {
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(user);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't insert User: "
+                    + user.toString(), e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public List<User> listUsers() {
+        try (Session session = sessionFactory.openSession()) {
+            return session.createQuery("from User", User.class).getResultList();
+        }
+    }
+}
